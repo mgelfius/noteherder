d2048bb44e08ab3a38d9ba29d7f48edd7de592e3
Mark Gelfius <mgelfius@purdue.edu>
2017-07-10 14:27:24 -0400

    Sync notes only after authenticating

--- src/App.js
+++ src/App.js
@@ -78,7 +78,10 @@ class App extends Component {
   }
 
   handleAuth = (user) =>{
-    this.setState({uid : user.uid})
+    this.setState(
+        {uid : user.uid},
+        this.syncNotes
+      )
   }
 
   signOut = () =>{
