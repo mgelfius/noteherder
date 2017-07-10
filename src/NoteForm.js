2d36bcc432a00000be5dc3dfffe2acdf1c83df7c
Mark Gelfius <mgelfius@purdue.edu>
2017-07-10 15:36:41 -0400

    WIP Add rich text editing

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react'
+import RichTextEditor from 'react-rte'
 import './NoteForm.css'
 
 
 class NoteForm extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      editorValue: RichTextEditor.createEmptyValue(),
+    }
+  }
+
     handleChanges = (ev) =>{
       const note = {...this.props.currentNote}
       note[ev.target.name] = ev.target.value
       this.props.saveNote(note)
+    }
+
+    handleEditorChanges = (editorValue) =>{
+      this.setState({ editorValue })
+      const note = {...this.props.currentNote}
+      note.body = editorValue.toString('html')
+      this.props.saveNote(note)
+    }
+
+    componentWillReceiveProps = (nextProps) =>{
+      
     }
 
     render(props){
@@ -31,12 +50,12 @@ class NoteForm extends Component{
                 onChange = {this.handleChanges}/>
             </p>
             
-            <textarea 
+            <RichTextEditor 
               name="body" 
               placeholder="Type your note here"
-              value= {this.props.currentNote.body}
-              onChange = {this.handleChanges}>
-            </textarea>
+              value= {this.state.editorValue}
+              onChange = {this.handleEditorChanges}>
+            </RichTextEditor>
           </form>
           
         </div>
