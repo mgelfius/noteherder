c08fc62950458cd44c9c4d00a9f839a1ca15b6b5
Mark Gelfius <mgelfius@purdue.edu>
2017-07-13 13:49:40 -0400

    Sort notes by last edit time

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,22 +1,28 @@
 import React from 'react'
+
 import './NoteList.css'
 import Note from './Note'
 
+const NoteList = ({ notes }) => {
+  const sortNotes = (a, b) => {
+    return (notes[b].updatedAt || 0) - (notes[a].updatedAt || 0)
+  }
 
-const NoteList = (props) =>{
-    const noteIds = Object.keys(props.notes)
-    return(
-        <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            {noteIds.map(noteId => 
-              <Note note = {props.notes[noteId]} 
-              key={noteId} 
-            />)}
-          </ul>
-        </div>
-    )
+  const noteIds = Object.keys(notes).sort(sortNotes)
+
+  return (
+    <div className="NoteList">
+      <h3>Notes</h3>
+      <ul id="notes">
+        {noteIds.map(noteId => (
+          <Note
+            key={noteId}
+            note={notes[noteId]}
+          />
+        ))}
+      </ul>
+    </div>
+  )
 }
-
 
 export default NoteList
